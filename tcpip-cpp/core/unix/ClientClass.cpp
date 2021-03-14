5ad41b093126a7cc9a5b9aafa3a20b2352159de5
Robin.Mueller <robin.mueller.m@gmail.com>
2021-03-14 01:45:01 +0100

    updated unix stuff

--- tcpip-cpp/core/unix/ClientClass.cpp
+++ tcpip-cpp/core/unix/ClientClass.cpp
@@ -1,20 +1,16 @@
 #include "../ClientClass.h"
 
+#include <unistd.h>
+
 #include <iostream>
 
-ClientClass::ClientClass(tcpip::DemoModes mode): mode(mode),
+ClientClass::ClientClass(tcpip::DemoConfig& cfg): mode(cfg.mode),
+        server_address(cfg.server_address), server_port(cfg.server_port),
         reception_buffer(tcpip::BUFFER_SIZES) {
-    WSADATA wsaData;
-    // Initialize Winsock
-    int retval = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (retval != 0) {
-        std::cerr << "WSAStartup failed with error: " << retval << std::endl;
-        throw std::runtime_error("WSAStartup failed!");
-    }
 }
 
 ClientClass::~ClientClass() {
-    closesocket(connect_socket);
+    close(connect_socket);
 
 }
 
